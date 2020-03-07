0fbf5ac694b022666e1effdc999af75aa4957680
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-03-07 21:07:27 +0000

    successfully install and tested babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var babel = require('gulp-babel');
 
 function sassCompile(cb) {
     return gulp.src('app/scss/**/*.scss')
@@ -11,6 +12,14 @@ function sassCompile(cb) {
 function watch() {
     gulp.watch('app/scss/**/*.scss', sassCompile);
 }
+
+gulp.task('babel', () =>
+    gulp.src('app/js/es6/**/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('app/js/es5'))
+);
 
 exports.sass = sassCompile;
 exports.watch = watch;
