69824a1b629d289d40bebc3fd77e0ae85105c73c
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-03-21 20:15:16 +0000

    created dynamic handlebars to display projects, and restyled appropriately. Amended json file to only have whats needed

--- app/js/es5/clickEvents.js
+++ app/js/es5/clickEvents.js
@@ -10,16 +10,5 @@ $(document).ready(function () {
   document.getElementById('skill-btn').addEventListener('click', function () {
     document.scrollingElement.scrollTop = 1525;
     return document.scrollingElement.scrollTop;
-  }); //Animate project Learn More event
-
-  document.getElementById('learn-more-btn-1').addEventListener('click', function () {
-    $('#poster-body-image-1').animate({
-      opacity: '1'
-    }, 600);
-  });
-  document.getElementById('project-close-btn-1').addEventListener('click', function () {
-    $('#poster-body-image-1').animate({
-      opacity: '0'
-    }, 600);
   });
 });
--- app/js/es5/projectsFetch.js
+++ app/js/es5/projectsFetch.js
@@ -1,1 +1,13 @@
 "use strict";
+
+fetch('../projects.json').then(function (data) {
+  return data.json();
+}).then(function (fetchedProjects) {
+  fetch('hand.hbs').then(function (handData) {
+    return handData.text();
+  }).then(function (handData) {
+    var hbsTemplate = Handlebars.compile(handData);
+    var projects = hbsTemplate(fetchedProjects);
+    document.querySelector('#main-projects-box').innerHTML = projects;
+  });
+});
