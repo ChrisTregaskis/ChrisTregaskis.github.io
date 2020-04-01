4f408a4d3a1654e14bf2d7ea86e46b5be813ea7e
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-01 22:17:13 +0100

    removed skills canvas and script link to skills js, added containers for skills imgs and event handlers for buttons

--- src/js/es6/clickEvents.js
+++ src/js/es6/clickEvents.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 
-    //** For skills section click events, refer to skills.js **//
+    var opacityTransitionTime = 'opacity 1s';
+    var skillImages_JS = $('#skills-js-box');
+    var skillImages_PHP = $('#skills-php-box');
+    var skillImages_FE = $('#skills-fe-box');
+    var skillImages_Tools = $('#skills-tools-box');
+
 
     //About me
     document.getElementById('anchor-about-me').addEventListener('click', function() {
@@ -35,5 +40,76 @@ $(document).ready(function() {
         return document.scrollingElement.scrollTop
     });
 
+    document.getElementById('skill-js-btn').addEventListener('click', function() {
+
+        skillImages_JS.css('display', 'block');
+        skillImages_JS.css('opacity', '1');
+        skillImages_JS.css('transition', opacityTransitionTime);
+
+        skillImages_PHP.css('opacity', '0');
+        skillImages_PHP.css('transition', opacityTransitionTime);
+        skillImages_FE.css('opacity', '0');
+        skillImages_FE.css('transition', opacityTransitionTime);
+        skillImages_Tools.css('opacity', '0');
+        skillImages_Tools.css('transition', opacityTransitionTime);
+
+        skillImages_PHP.css('display', 'none');
+        skillImages_FE.css('display', 'none');
+        skillImages_Tools.css('display', 'none');
+    });
+
+    document.getElementById('skill-php-btn').addEventListener('click', function () {
+
+        skillImages_PHP.css('display', 'block');
+        skillImages_PHP.css('opacity', '1');
+        skillImages_PHP.css('transition', opacityTransitionTime);
+
+        skillImages_JS.css('opacity', '0');
+        skillImages_JS.css('transition', opacityTransitionTime);
+        skillImages_FE.css('opacity', '0');
+        skillImages_FE.css('transition', opacityTransitionTime);
+        skillImages_Tools.css('opacity', '0');
+        skillImages_Tools.css('transition', opacityTransitionTime);
+
+        skillImages_JS.css('display', 'none');
+        skillImages_FE.css('display', 'none');
+        skillImages_Tools.css('display', 'none');
+    });
+
+    document.getElementById('skill-fe-btn').addEventListener('click', function () {
+
+        skillImages_FE.css('display', 'block');
+        skillImages_FE.css('opacity', '1');
+        skillImages_FE.css('transition', opacityTransitionTime);
+
+        skillImages_JS.css('opacity', '0');
+        skillImages_JS.css('transition', opacityTransitionTime);
+        skillImages_PHP.css('opacity', '0');
+        skillImages_PHP.css('transition', opacityTransitionTime);
+        skillImages_Tools.css('opacity', '0');
+        skillImages_Tools.css('transition', opacityTransitionTime);
+
+        skillImages_JS.css('display', 'none');
+        skillImages_PHP.css('display', 'none');
+        skillImages_Tools.css('display', 'none');
+    });
+
+    document.getElementById('skill-tools-btn').addEventListener('click', function () {
+
+        skillImages_Tools.css('display', 'block');
+        skillImages_Tools.css('opacity', '1');
+        skillImages_Tools.css('transition', opacityTransitionTime);
+
+        skillImages_JS.css('opacity', '0');
+        skillImages_JS.css('transition', opacityTransitionTime);
+        skillImages_PHP.css('opacity', '0');
+        skillImages_PHP.css('transition', opacityTransitionTime);
+        skillImages_FE.css('opacity', '0');
+        skillImages_FE.css('transition', opacityTransitionTime);
+
+        skillImages_JS.css('display', 'none');
+        skillImages_PHP.css('display', 'none');
+        skillImages_FE.css('display', 'none');
+    })
 
 });
